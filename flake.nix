c9deda594529117c462d747024f6da2566a1cdbb
Kristoffer Dalby <kristoffer@dalby.cc>
2026-08-25 20:39:50 +0000

    nix: move off deprecated stdenv.is* and pkgs.system aliases

--- flake.nix
+++ flake.nix
@@ -121,7 +121,7 @@
           );
         }
         # NixOS module evaluation needs a Linux system.
-        // pkgs.lib.optionalAttrs pkgs.stdenv.isLinux {
+        // pkgs.lib.optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
           module-eval = import ./module-eval.nix {
             inherit
               pkgs
